180f0af64ea5b55351d33d56dea0fddeff300250
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2023-10-10 13:31:52 -0400

    initial commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+// age group 0-19
+let total1 = 15 + 16 + 17;
+let average1 = (15 + 16 + 17) / 3;
+console.log(`Case data for population age group 0-19: total=${total1} average=${average1}`);
+
+// age group 20-39
+let total2 = 56 + 2 + 67;
+let average2 = (56 + 2 + 67) / 3;
+console.log(`Case data for population age group 20-39: total=${total2} average=${average2}`);
